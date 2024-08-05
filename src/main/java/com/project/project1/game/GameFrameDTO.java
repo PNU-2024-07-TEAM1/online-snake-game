69a40a116467249047a632e497893fa299173cfc
mandu <jys0972@gmail.com>
2024-08-05 17:21:58 +0900

    feat(GameFrameDTO.java) : frame 마다 보낼 dto 추가

--- src/main/java/com/project/project1/game/GameFrameDTO.java
+++ src/main/java/com/project/project1/game/GameFrameDTO.java
@@ -1,4 +1,13 @@
 package com.project.project1.game;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
 public class GameFrameDTO {
+    private boolean isAlive;
+    private List<Snake> snakes;
 }
