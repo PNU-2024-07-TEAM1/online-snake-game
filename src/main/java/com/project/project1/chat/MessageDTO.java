3f16a5102b2c83cb53efd5d99e8eb8c7c5ba7b0f
mandu <jys0972@gmail.com>
2024-08-05 14:47:05 +0900

    feat(MessageDTO.java) : dto 구현

---
+++ src/main/java/com/project/project1/chat/MessageDTO.java
@@ -0,0 +1,11 @@
+package com.project.project1.chat;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class MessageDTO{
+    private String username;
+    private String content;
+}
