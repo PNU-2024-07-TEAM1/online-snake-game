ba6bda8492782deadd83056df2d4eb5e466a8458
mandu <jys0972@gmail.com>
2024-08-05 13:57:21 +0900

    feat(ChatService.java) : entity를 사용하도록 service 수정

--- src/main/java/com/project/project1/chat/ChatService.java
+++ src/main/java/com/project/project1/chat/ChatService.java
@@ -1,18 +1,27 @@
 package com.project.project1.chat;
 
+import com.project.project1.member.Member;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
+@RequiredArgsConstructor
 @Service
 public class ChatService {
     public static List<String> messages= new ArrayList<>();
+    private final MessageRepository messageRepository;
 
-    public void addMessage(String message){
-        messages.add(message);
+    public void addMessage(Member sender, String content){
+        Message message = new Message();
+        message.setSender(sender);
+        message.setContent(content);
+        messageRepository.save(message);
     }
 
-    public List<String> getMessages(){
+    public List<Message> getMessages(){
+        List<Message> messages = messageRepository.findAll();
         return messages;
     }
 }
