029d44ec82278b6f371aa59ae233190c956f336e
mandu <jys0972@gmail.com>
2024-08-05 17:21:37 +0900

    feat(GameController.java) : 라우터 추가

--- src/main/java/com/project/project1/game/GameController.java
+++ src/main/java/com/project/project1/game/GameController.java
@@ -1,4 +1,32 @@
 package com.project.project1.game;
 
+import com.project.project1.member.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
+
+@RequiredArgsConstructor
+@Controller
+@RequestMapping("/game")
 public class GameController {
+
+    private final GameService gameService;
+    private final MemberService memberService;
+    @GetMapping("snake")
+    public String getGamePage(){
+        return "game_page";
+    }
+
+    @MessageMapping("/gameInput")
+    public void gameInput(String direction, Principal principal) throws Exception {
+        Snake snake = gameService.getSnake(
+                memberService.getMember(principal.getName()).getId()
+        );
+        snake.setDirection(direction);
+    }
+
 }
