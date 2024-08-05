7b259191e54c72771cd0642ed84628cf53f1adb0
mandu <jys0972@gmail.com>
2024-08-05 17:22:18 +0900

    feat(GameService.java) : 간단한 게임 로직 구현

--- src/main/java/com/project/project1/game/GameService.java
+++ src/main/java/com/project/project1/game/GameService.java
@@ -1,4 +1,33 @@
 package com.project.project1.game;
 
+import com.project.project1.member.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class GameService {
+
+    private final int DEFAULT_SNAKE_LENGTH = 3;
+    private static List<Snake> snakes = new ArrayList<Snake>();
+
+
+
+    void addSnake(Member member){
+        Snake snake = new Snake(member.getId());
+        snakes.add(snake);
+    }
+
+    Snake getSnake(Integer memberId) throws Exception {
+        for (Snake snake : snakes){
+            if (snake.getMemberId().equals(memberId)){
+                return snake;
+            }
+        }
+        throw new Exception("Snake not found");
+    }
 }
+
