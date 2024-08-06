221a1f8e89452ceef6fb185634f55ceaf89fe0d9
mandu <jys0972@gmail.com>
2024-08-06 13:03:05 +0900

    feat(GameFrameDTO.java) : 통합된 DTO를 위한 수정

--- src/main/java/com/project/project1/game/GameFrameDTO.java
+++ src/main/java/com/project/project1/game/GameFrameDTO.java
@@ -8,8 +8,6 @@ import java.util.List;
 @Getter
 @Setter
 public class GameFrameDTO {
-    private Integer playerId;
-    private boolean isAlive;
     private int score;
     private List<Snake> snakes;
 }
