bfd98b7e9dea9a61da48b26ecc6d275fde55e44b
sinsehwan <91408891+sinsehwan@users.noreply.github.com>
2024-08-07 15:25:20 +0900

    게임 부드럽게 만들기 feature/60 (#68)
    * save changes
    
    * check
    
    * 프레임 2배
    
    * feat(Snake.java) : snake에 username 추가
    
    * fix(GameController.java) : username 으로 찾게 변경
    
    * save
    
    * feat(MemberService.java) : id로 member 찾기 추가
    
    * a
    
    * save
    
    * ìgame 부드럽게 만들기 head요소  추가
    
    ---------
    
    Co-authored-by: mandu <jys0972@gmail.com>

--- src/main/resources/static/game/snake.js
+++ src/main/resources/static/game/snake.js
@@ -101,7 +101,7 @@ class Snake {
         // Update the position based on the current direction
         // First, create a copy of the current head to be updated
         //let head = { ...this.snakeNodePlaces[0]};
-
+/*
         let firstSegment = this.snakeNodePlaces[0];
 
         switch (this.direction) {
@@ -117,6 +117,33 @@ class Snake {
             case 'down':
                 this.snakeNodePlaces[0] = { x: firstSegment.x, y: firstSegment.y + 0.1};
                 break;
+        }
+*/
+
+        let newHead = { ...this.snakeNodePlaces[0] };
+
+        switch (this.direction) {
+            case 'left':
+                newHead.x -= 0.1;
+                break;
+            case 'right':
+                newHead.x += 0.1;
+                break;
+            case 'up':
+                newHead.y -= 0.1;
+                break;
+            case 'down':
+                newHead.y += 0.1;
+                break;
+        }
+
+        // 새로운 머리 노드를 배열 앞에 추가합니다.
+        this.snakeNodePlaces.unshift(newHead);
+
+        // 이전 프레임 head 노드 제거
+        if (this.snakeNodePlaces.length > this.snakeLength + 1)
+        {
+            this.snakeNodePlaces.splice(1, 1)
         }
 
         if (this.snakeNodePlaces.length > 1) {
@@ -287,7 +314,7 @@ function fetchGameFrame() {
 // const gameFrameDTO = fetchGameFrame();
 // drawGameFrame(gameFrameDTO);
 
-let toggle = 10;
+let toggle = 9;
 
 /*
 setInterval(() => {
@@ -303,15 +330,22 @@ setInterval(() => {
 }, 25);*/
 
 let lastUpdateTime = 0;
-const updateInterval = 25; // 애니메이션 업데이트 주기 (ms)
-const serverUpdateInterval = 500; // 서버와의 데이터 동기화 주기 (ms)
+const updateInterval = 30; // 애니메이션 업데이트 주기 (ms)
+const serverUpdateInterval = 270; // 서버와의 데이터 동기화 주기 (ms)
 
 function gameLoop(timestamp) {
     // 애니메이션 업데이트
     if (timestamp - lastUpdateTime >= updateInterval) {
         lastUpdateTime = timestamp;
+        if (toggle !== 10)
+        {
+            drawGameFrame_local(); // Local update
+            toggle += 1;
+        }
+        else{
+            toggle = 1;
+        }
 
-        drawGameFrame_local(); // Local update
     }
 
     requestAnimationFrame(gameLoop); // 다음 프레임을 요청
