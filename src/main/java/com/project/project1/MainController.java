8f6ecc26e57df38b5a8335b89c2c07d7ca8f8ea7
mandu <jys0972@gmail.com>
2024-08-07 14:11:47 +0900

    feat(MainController.java) : 참조 방식 변경, 설정 라우터 추가

--- src/main/java/com/project/project1/MainController.java
+++ src/main/java/com/project/project1/MainController.java
@@ -8,10 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import jakarta.validation.Valid;
-import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -30,20 +29,58 @@ public class MainController {
     }
 
     @GetMapping("/main")
-    public String main(Model model){
+    public String main(Model model) throws Exception {
         List<Member> memberList = memberRepository.findAllByOrderByScoreDesc();
         model.addAttribute("memberList", memberList);
 
         Object princial = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         CustomOAuth2UserDetails customOAuth2UserDetails = (CustomOAuth2UserDetails) princial;
 
-        String curUserName = customOAuth2UserDetails.getUsername();
+        String curUserName = memberRepository.findByLoginId(
+                customOAuth2UserDetails.getLoginId()
+        ).get().getUsername();
         String curUserProfileImage = customOAuth2UserDetails.getProfileImage();
         System.out.println(curUserProfileImage);
+
+        Member member = memberService.getMember(curUserName);
 
         model.addAttribute("curUserProfileImage", curUserProfileImage);
         model.addAttribute("curUserName", curUserName);
 
+        String color = member.getColor();
+        if (color == null){
+            color = "#000000";
+        }
+        model.addAttribute("curColor", color);
+        return "main";
+    }
+
+    @PostMapping("/edit")
+    public String editUserSetting(Model model, @RequestParam(value = "username") String newUsername, @RequestParam(value = "color") String newColor) throws Exception {
+        Object princial = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        CustomOAuth2UserDetails customOAuth2UserDetails = (CustomOAuth2UserDetails) princial;
+
+        String userLoginID = customOAuth2UserDetails.getLoginId();
+        String curUserName = memberRepository.findByLoginId(
+                userLoginID
+        ).get().getUsername();
+        memberService.editSetting(curUserName, newUsername, newColor);
+
+        List<Member> memberList = memberRepository.findAllByOrderByScoreDesc();
+        model.addAttribute("memberList", memberList);
+        curUserName = memberRepository.findByLoginId(
+                userLoginID
+        ).get().getUsername();
+        String curUserProfileImage = customOAuth2UserDetails.getProfileImage();
+        model.addAttribute("curUserProfileImage", curUserProfileImage);
+        model.addAttribute("curUserName", curUserName);
+
+        Member member = memberRepository.findByLoginId(userLoginID).get();
+        String color = member.getColor();
+        if (color == null){
+            color = "#000000";
+        }
+        model.addAttribute("curColor", color);
         return "main";
     }
 
