26a8a2f7a1b8ba0ae95b3abb497279266c0c9cc2
mandu <88422717+YunseongJeong@users.noreply.github.com>
2024-08-09 12:39:03 +0900

    chat system 디비 종속성 제거 (#101)
    * refact(message 디비) : 디비 관련 삭제
    
    * feat(ChatController.java) : 디비 종속성 제거

--- src/main/java/com/project/project1/chat/ChatController.java
+++ src/main/java/com/project/project1/chat/ChatController.java
@@ -7,7 +7,6 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.security.Principal;
@@ -18,14 +17,8 @@ import java.util.List;
 @RequestMapping("/chat")
 public class ChatController {
     private final SimpMessagingTemplate template;
-    private final ChatService chatService;
     private final MemberService memberService;
 
-    @GetMapping("/api/items")
-    public List<Message> getItems() {
-        // 실제 데이터 리스트를 반환합니다
-        return chatService.getMessages();
-    }
 
     @MessageMapping("/updateItems")
     public void updateItems(List<String> items) {
@@ -38,7 +31,6 @@ public class ChatController {
     @SendTo("/topic/messages") // 메시지를 /topic/messages로 브로드캐스팅
     public MessageDTO sendMessage(String message, Principal principal) throws Exception {
 
-        Message messageEntity;
         if (message.equals("/in")){
             MessageDTO messageDTO = new MessageDTO();
             messageDTO.setUsername(principal.getName());
@@ -47,15 +39,10 @@ public class ChatController {
 
         }
         Member member = memberService.getMember(principal.getName());
-        messageEntity = chatService.makeMessage(
-                member,
-                message
-        );
-        chatService.addMessage(messageEntity);
 
         MessageDTO messageDTO = new MessageDTO();
-        messageDTO.setUsername(messageEntity.getSender().getUsername());
-        messageDTO.setContent(messageEntity.getContent());
+        messageDTO.setUsername(principal.getName());
+        messageDTO.setContent(message);
         messageDTO.setColor(member.getColor());
         return messageDTO;
     }
