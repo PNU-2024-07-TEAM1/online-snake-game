68da40524af7a16094c9510c16953c1fdfd77b6b
mandu <88422717+YunseongJeong@users.noreply.github.com>
2024-08-09 12:39:03 +0900

    chat system 디비 종속성 제거 (#101)
    * refact(message 디비) : 디비 관련 삭제
    
    * feat(ChatController.java) : 디비 종속성 제거

--- src/main/java/com/project/project1/chat/MessageRepository.java
+++
@@ -1,6 +0,0 @@
-package com.project.project1.chat;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MessageRepository extends JpaRepository<Message, Integer> {
-}
